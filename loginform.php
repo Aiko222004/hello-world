36c78a2cece6d848f9fb93085260f399c77260c7
Aiko222004 <115717161+Aiko222004@users.noreply.github.com>
2025-11-28 12:49:25 +0800

    merging try

--- loginform.php
+++ loginform.php
@@ -142,7 +142,7 @@
     <div class="login-container">
         <div class="accent-bar"></div>
         <h2>🎫 Ticketing System</h2>
-        
+        <h2>Login to Your Account</h2>
         <?php if(isset($_SESSION['success'])): ?>
             <div class="success-message">
                 <?php 
